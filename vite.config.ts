8fd738458b8fa6b79109ed357aa66143c60287b3
Elias Rivnenko <jiles@ya.ru>
2026-03-12 12:59:38 +0300

    конфиг сборки

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Замени 'dasha-signals-visualization' на название своего репозитория
-  base: '/dasha-signals-visualization/',
+  base: '/dasha_signals_visualization/',
 })
